8c22f61d4b37849d1de83c0d01fa10fa80420cf3
gougeond <136575967+gougeond@users.noreply.github.com>
2023-06-14 13:52:04 +0200

    Update streamlit_app.py
    ajout de texte

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
 
-streamlit.title('Badge2 snowflake')
-streamlit.header('Entête')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
