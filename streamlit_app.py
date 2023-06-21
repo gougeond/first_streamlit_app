ce6f02cf2764221fbee3ed05762dce5d82e9bfb4
gougeond <136575967+gougeond@users.noreply.github.com>
2023-06-21 11:02:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,4 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit=streamlit.multiselect("What fruit would you like to add ?", list(my_fruit_list.index))
 
 add_my_fruit = my_fruit_list.loc[fruits_selected]
-streamlit.text('Thanks for adding jackfruit')
+streamlit.text('Thanks for adding',add_my_fruit)
