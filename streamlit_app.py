ec05d84b9b953904fda6fd1e46bd898cffb1f190
gougeond <136575967+gougeond@users.noreply.github.com>
2023-06-21 17:14:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,25 +22,25 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-      streamlit.error("please select a fruit to get information.")
-  else:
-    back_from_function = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)  
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("please select a fruit to get information.")
+    else:
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)  
 except URLError as e:
     streamlit.error()
 
 add_my_fruit=streamlit.text_input("What fruit would you like to add ?")
 streamlit.write('Thanks for adding',add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from rivery')")
 
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
@@ -53,5 +53,4 @@ if streamlit.button('Get Fruit load list'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-#my_cur.execute("insert into fruit_load_list values ('from rivery')")
 
